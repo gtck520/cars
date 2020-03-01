55d670f3624b33dd8fe60d842ed989148b2ee64d
gtck520 <371609974@qq.com>
2020-03-01 15:33:30 +0800

    666

--- car/application/controller/www/Index.php
+++ car/application/controller/www/Index.php
@@ -11,7 +11,7 @@ class Index
      * @OA\Info(title="车塘小程序API", version="0.1")
      */
     public function index(){
-        echo "5555555555";exit;
+        echo "66666666";exit;
         $req=G();
         if(isset($req['type'])&&$req['type']=="admin"){
 
