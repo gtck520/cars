aa2aa9286e65fe204c49d8499f1ce55281e79d9d
gtck520 <371609974@qq.com>
2020-03-01 16:40:51 +0800

    注释文档

--- car/application/controller/www/Login.php
+++ car/application/controller/www/Login.php
@@ -10,7 +10,37 @@ use app\validate\User as UserValidate;
 
 class Login
 {
-    //登录
+    /**
+     * @OA\Post(
+     *     path="/logins",
+     *     tags={"用户中心"},
+     *     summary="用户登录",
+     *     @OA\Parameter(name="Content-Type",in="header",example="application/x-www-form-urlencoded",required=true),
+     *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="application/x-www-form-urlencoded",
+     *             @OA\Schema(
+     *                 type="object",
+     *                 @OA\Property(property="code",type="string",description="编码"),
+     *                 @OA\Property(property="mobile",type="string",description="手机号"),
+     *                 @OA\Property(property="avatar",type="string",description="头像"),
+     *                 @OA\Property(property="nickname",type="string",description="昵称"),
+     *                 example={"vin": "sadf656s4df6465","mobile": "18695732896","avatar": "123.jpg","vin": "老牛逼了"}
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *       response=200,
+     *       description="请求成功",
+     *           @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(ref="#/components/schemas/query_getpay"),
+     *          ),
+     *     ),
+     *     @OA\Response(response=401,description="权限验证失败"),
+     *     @OA\Response(response=400,description="请求失败")
+     * )
+     */
     public function login(){
         $req = P();
         LoginValidate::checkInput($req);
