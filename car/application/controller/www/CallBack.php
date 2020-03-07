c9c538a3875b797b8d464a2fcfc401c7d8870533
gtck520 <371609974@qq.com>
2020-03-07 11:39:51 +0800

    1

--- car/application/controller/www/CallBack.php
+++ car/application/controller/www/CallBack.php
@@ -10,11 +10,11 @@ class CallBack
 {
     //维保回调
     public function maintenance(){
-        $req = P();
+        $result= file_get_contents("php://input");//获取post原始数据
         $reqg= G();
         //Common::checkVin($req);
         $req['orderid'] =$reqg['orderid'];
-        Log::write(json_encode($req) . ':' . date('Y-m-d H-i-s'), 'maintenance.log',  'callback_query');
+        Log::write($result . ':' . date('Y-m-d H-i-s'), 'maintenance.log',  'callback_query');
 //        if($req['code']==888){
 //            $detail_data['order_id']=$req['orderid'];
 //            $detail_data['vin']=$req['vin'];
