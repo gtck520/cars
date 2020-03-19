5539b6e734b60d228a2ab161c571b3a4dff3effb
gtck520 <371609974@qq.com>
2020-03-19 22:45:08 +0800

    代理权限验证方法

--- car-manager/application/controller/www/AdminController.php
+++ car-manager/application/controller/www/AdminController.php
@@ -8,6 +8,8 @@ use app\cache\Token;
 use app\model\Admin as ManAdminModel;
 use app\model\ManPower as ManPowerModel;
 use app\model\ManRole as ManRoleModel;
+use app\model\AgentCity as AgentCityModel;
+use app\model\CityArea as CityAreaModel;
 use king\core\Route;
 
 class AdminController
@@ -80,5 +82,51 @@ class AdminController
             }
             if (!$flag) Response::SendResponseJson(401, '您没有操作权限');
         }
+    }
+    //校验代理人权限
+    protected static function checkAgent($req){
+        $current_city=0;
+        if(!empty($req["province_id"])){
+            $current_city=$req["province_id"];
+            $back_city['province_id']=$req["province_id"];
+        }
+        if(!empty($req["city_id"])){
+            $current_city=$req["city_id"];
+            $back_city['city_id']=$req["city_id"];
+        }
+        if(!empty($req["area_id"])){
+            $current_city=$req["area_id"];
+            $back_city['area_id']=$req["area_id"];
+        }
+        //判断是否代理人，是的话取默认代理人城市权限， 并且切换城市需校验权限
+        $admin_id = self::loginId();
+        $where = ['id' => self::$admin_id];
+        $adminArr = ManAdminModel::where($where)->find();
+        if($adminArr['is_agent']==1){
+            $agents=AgentCityModel::where(['admin_id'=>$admin_id])->get();
+            foreach ($agents as $key=>$value){
+                $result=CityAreaModel::where('path', 'like', '%' . $value['agent_city_id'] . '%')->get();
+                $agents_citys=carray_column($result,'id');
+
+                if(in_array($current_city,$agents_citys)){//当前城市存在于已拥有的权限中，验证通过返回所筛选的城市
+                   break;
+                }else{
+                    if($current_city==0){
+                        $default_city=CityAreaModel::field(['path'])->where(['id'=>$value['agent_city_id']])->find();
+                        $city_array=explode(',',$default_city['path']);
+                        $back_city['province_id'] = $city_array[0] ?? '';
+                        $back_city['city_id'] = $city_array[1] ?? '';
+                        $back_city['area_id'] = $city_array[2] ?? '';
+                    }else{
+                        $back_city=false;
+                    }
+                }
+            }
+         }
+
+        return $back_city;
+
+
+
     }
 }
